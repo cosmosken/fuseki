8b5e40db9af17b2f9ade45a519bd972f11edfd77
qmq <minqianqiang@gmail.com>
2024-09-04 22:52:37 +0800

    init

---
+++ src/main/java/com/bu/dong/fuseki/FusekiApplication.java
@@ -0,0 +1,13 @@
+package com.bu.dong.fuseki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FusekiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FusekiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bu/dong/fuseki/FusekiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bu.dong.fuseki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FusekiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
